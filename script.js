6e7a56440be087b36a0330d325bcbb32c028518d
AlexMrsh <alexis.delahaye7@gmail.com>
2022-11-25 00:00:26 +0100

    done own version

--- script.js
+++ script.js
@@ -1,71 +1,93 @@
-/*
-const createPlayer = (name, playerType) => {
-    (function confirmCreation(){
-        console.log(`Player ${playerType} : ${name} have been created`)
-    })()
-
-    return{name}
-}
-
-player1 = createPlayer('Alex', 'X')
-player2 = createPlayer('AI', 'O')
-*/
-
-const CROSS_SYMBOL = 'X'
-const CIRCLE_SYMBOL = 'O'
-let circleTurn = false
-
-
 const gameBoard = (() => {
-    const _board = new Array(9);
-    let currentSymbol = 'X';
+    const CIRCLE_CLASS = 'circle'
+    const CROSS_CLASS = 'close'
+    const WINNING_COMBINATION = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
 
-    const initializeField = () => {
-        const container = document.getElementById('container');
-        for(let i = 0; i < _board.length; i++){
+    let circleTurn
+
+    const field = document.getElementById('field')
+    const fieldElements = field.querySelectorAll('div')
+    const roundResultBackground = document.getElementById('round-result-background')
+    const playAgainButton = document.getElementById('new-round-button')
+
+    playAgainButton.addEventListener('click', newRound)
+
+    function startGame(){
+        field.innerHTML = ''            //reset field content
+        for(i = 0; i < 9; i++){         //create field grid
             let fieldDiv = document.createElement('div')
-            fieldDiv.setAttribute('data-boardindex', i);
+            fieldDiv.setAttribute('data-cellindex', i)
+            fieldDiv.classList.add('material-symbols-outlined')
             fieldDiv.addEventListener('click', handleClick, {once: true})
-            container.appendChild(fieldDiv)
+            field.appendChild(fieldDiv)
+        }
+        circleTurn = false;
+    }
+
+    function handleClick(e){
+        cell = e.target
+        let currentSymbol = circleTurn ? CIRCLE_CLASS : CROSS_CLASS
+
+        if(currentSymbol === CIRCLE_CLASS){
+            e.target.textContent = 'circle'
+        }else{
+            e.target.textContent = 'close'
+        }
+        
+        if(checkWin(currentSymbol)){
+            showRoundResult(true, currentSymbol)        //true == someone won
+            //update scores et roundCount
+        }else if(checkFull()){
+            showRoundResult(false)                      //false == no winner
+        }else{
+            circleTurn = !circleTurn
         }
     }
-
-    const handleClick = event => {
-        const boardElement = e.target;
-        const currentSymbol = circleTurn ? CIRCLE_SYMBOL : CROSS_SYMBOL
-        let boardIndex = event.target.getAttribute('data-boardindex')
-        boardElement.textContent = currentSymbol;
-        
-        _board[boardIndex] = currentSymbol;
-        event.target.textContent = currentSymbol;
-        
-        checkGameStatus();
-        //computerTurn
-        console.log(_board)
-    }
-
-    const checkGameStatus = () => {
-        const winingCombinations = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ];
-        return winingCombinations.some(combination => {
+    
+    function checkWin(symbol){
+        return WINNING_COMBINATION.some(combination => {
             return combination.every(index => {
-                const boardElements = document.querySelectorAll('[data-boardindex]')
-                return boardElements[index].textContent
+                let cellElements = document.querySelectorAll('[data-cellindex]')
+                return cellElements[index].textContent === symbol
             })
         })
     }
 
-    return {
-        initializeField
+    function checkFull(){
+        let cellElements = document.querySelectorAll('[data-cellindex]')
+        return [...cellElements].every(element => element.textContent !== '')
+    }
+
+    function showRoundResult(roundResult, currentSymbol){
+        roundResultBackground.classList.remove('hidden')
+        let resultText = document.getElementById('result-text')
+
+        if(roundResult){
+            let roundWinner
+            (currentSymbol === CIRCLE_CLASS) ? roundWinner = '0' : roundWinner = 'X'
+            resultText.textContent = `${roundWinner} won the round`
+        }else{
+            resultText.textContent = 'No winner'
+        }
+    }
+
+    function newRound(){
+        roundResultBackground.classList.add('hidden')
+        startGame()
+    }
+
+    return{
+        startGame
     }
 })()
 
-gameBoard.initializeField()
+gameBoard.startGame()
